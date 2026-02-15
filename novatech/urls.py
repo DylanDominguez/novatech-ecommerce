ba3f007c219adb2927bf38551954f9f29f7b5763
DylanDominguez <dylandomperez1802@gmail.com>
2026-02-14 19:02:23 -0500

    descripcion de productos

--- novatech/urls.py
+++ novatech/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('<slug:categoria_slug>/', views.productos_por_categoria, name='productos_por_categoria'),
+    path('producto/<int:pk>/', views.producto_detalle, name='producto_detalle'),
 ]
--- novatech/views.py
+++ novatech/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Categoria, Producto
 from django.core.paginator import Paginator
 
@@ -12,3 +12,30 @@ def index(request):
         "productos": obj_pagina,
     }
     return render(request, 'novatech/index.html', context)
+
+def productos_por_categoria(request, categoria_slug = None):
+    categoria_producto = None
+    productos = None
+    
+    if categoria_slug != None:
+        categoria_producto = get_object_or_404(Categoria, slug = categoria_slug)
+        productos = Producto.objects.filter(categoria=categoria_producto)
+    
+    paginator = Paginator(productos, 8)  # Mostrar 10 productos por página
+    num_pagina = request.GET.get('page')
+    obj_pagina = paginator.get_page(num_pagina)
+    
+    context = {
+        "productos": obj_pagina,
+    }
+    
+    return render(request, 'novatech/index.html', context)
+
+def producto_detalle(request, pk):
+    producto_detalle = get_object_or_404(Producto, pk = pk)
+    
+    context = {
+        "producto_detalle": producto_detalle,
+    }
+    
+    return render(request, 'novatech/producto_detalle.html', context)
